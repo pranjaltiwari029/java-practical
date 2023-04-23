4cd468111b934223fa256869645de7a053d672dd
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-04-23 16:30:03 +0530

    Create ques2.java

---
+++ pranjal_tiwari/javafile/ques2.java
@@ -0,0 +1,31 @@
+// WAP to print Fibonacci series.
+
+
+package pranjal_tiwari.javafile;
+import java.util.*;
+
+public class ques2 {
+    public static int fib(int n) {
+        if(n==1 ){
+            return 0;
+        }
+        if(n==2){
+            return 1;
+        }
+        else{
+            return fib(n-1) + fib(n-2);
+        }
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a number = ");
+        int num=sc.nextInt();
+        for(int i=1;i<=num+1;i++){
+            System.out.println(fib(i));
+        }
+
+        
+    }
+    
+}
