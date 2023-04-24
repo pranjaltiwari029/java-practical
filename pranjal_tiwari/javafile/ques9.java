65e1df1b69f5bd0431898276c5bb140c01be4114
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-04-24 09:46:02 +0530

    question 9

---
+++ pranjal_tiwari/javafile/ques9.java
@@ -0,0 +1,30 @@
+// package pranjal_tiwari.javafile;
+
+class overload{
+    public void show(){
+        System.out.println("implementing overloading function in java");
+    }
+    public void show(String Name){
+        System.out.println("student name = "+Name);
+    }
+}
+
+class override extends overload{
+    public void show(){
+        System.out.println("----------------------");
+        System.out.println("implementing overriding function in java" );
+        
+    }
+}
+
+public class ques9 {
+    public static void main(String[] args) {
+        overload obj=new overload();
+        obj.show();
+        obj.show("Pranjal");
+
+        override obj1=new override();
+        obj1.show();
+    }
+    
+}
