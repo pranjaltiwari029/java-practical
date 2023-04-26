45534cfbb3abf1572168042525fe1df5306f2810
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-04-26 23:40:42 +0530

    ques 6

---
+++ pranjal_tiwari/labwork/q6.java
@@ -0,0 +1,15 @@
+package pranjal_tiwari.labwork;
+
+class overloading{
+    overloading(){
+        System.out.println("using constructor overloading");
+    }
+}
+
+public class q6 {
+    
+    public static void main(String[] args) {
+        overloading obj =new overloading();
+        
+    }
+}
