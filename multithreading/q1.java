4cc1705857ae9c3749f12abcc04e09bc64b92a40
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-05-15 23:18:56 +0530

    Create q1.java

---
+++ multithreading/q1.java
@@ -0,0 +1,13 @@
+package multithreading;
+import java.util.*;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=1;i<11;i++){
+            System.out.println(a+"x"+i+" = "+a*i);
+        }
+    }
+    
+}
