bba776387e28c0a33218b77a5facc3146d4492b0
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-05-29 10:00:37 +0530

    Create q3.java

---
+++ pranjal_tiwari/labwork/threads/q3.java
@@ -0,0 +1,16 @@
+package threads;
+import  java.util.*;
+class mythread extends Thread{
+    int num;
+    mythread(int num){
+        this.num=num;
+        
+    }
+
+}
+public class q3 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
